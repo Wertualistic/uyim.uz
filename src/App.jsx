ed3db109309b8be62348380cb89c4497cab5ce75
Wertualistic <abduxalilovmuhammadali1@gmail.com>
2023-11-21 15:32:16 +0500

    Index

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,21 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Honadon from './Pages/Honadon/Honadon'
+import Main from './Pages/Main/Main'
+import Navbar from './layouts/Navbar/Navbar'
+import Footer from './layouts/Footer/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <Navbar />
       <Routes>
+        <Route path='/' element={<Main />} />
         <Route path='/honadon' element={<Honadon />} />
       </Routes>
+      <Footer />
     </div>
   )
 }
