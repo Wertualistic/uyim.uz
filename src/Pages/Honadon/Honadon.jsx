5d6bbcfdcc98fb1a67960a817d763f99adc069c4
UDinara <utamurodovadinara@gmail.com>
2023-11-21 15:15:14 +0500

    comment

---
+++ src/Pages/Honadon/Honadon.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import "../Honadon/Honadon.css"
+
+function Honadon() {
+  return (
+      <div className='honadon--container'>
+      <div className='honadon--header'>
+
+      {/*} <div className='container'>
+            <ul>
+               <li>Bosh sahifa</li>
+               <li>Biz haqimizda</li>
+               <li>Biz bilan bog’laning</li>
+               <li>+998 90 000-00-00</li>
+            </ul>
+        </div>*/}
+
+           <div className='honadon--main'>
+                <div className='honadon--box1'>
+                   <img src={Home}/>
+                </div>
+                <div className='honadon--box2'>
+                    <div className='box--section1'>
+                        <ul>
+                            <li>TK</li>
+                            <li>Qavati</li>
+                            <li>Maydoni</li>
+                            <li>Tayyor bo'lishi</li>
+                            <li>Narxi</li>
+                            <li>Raqami</li>
+                        </ul>
+                    </div>
+                    <div className='box--section2'>
+                        <ul>
+                            <li>Mirzo ulug'bek</li>
+                            <li>3/11</li>
+                            <li>192.72m<sup>2</sup></li>
+                            <li>12.29</li>
+                            <li>789 389 200 so’m</li>
+                            <li>#19</li>
+                        </ul>
+                    </div>
+                </div>
+           </div>
+
+           <div className='honadon--bottom'>
+              <button>
+              <img src={Vector}/>
+              Ortga qaytish</button>
+           </div>
+       </div>
+    </div>
+  )
+}
+
+export default Honadon
